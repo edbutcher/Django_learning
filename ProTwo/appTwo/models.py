6e4020d70c83cfc750c26a346c48c376b3eba88e
ваше имяedbutcher <edbutcher200@gmail.com>
2017-07-04 00:56:27 +0300

    Dj

--- ProTwo/appTwo/models.py
+++ ProTwo/appTwo/models.py
@@ -6,3 +6,6 @@ class User(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=200, unique=True)
 
+    def __str__(self):
+        return self.last_name + " " + self.first_name
+
